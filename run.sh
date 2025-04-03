56342f9609c815d85406328c9de83f9b3fa218a2
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-04-03 17:43:42 +0100

    Added port replacement

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ cd onepoint-chat-ui
 yarn
 yarn build
 
+# Change the port variable in .\onepoint-chat-ui\dist\index.html to 8084
+sed -i 's/4000/8084/g' dist/index.html
+sed -i 's/5000/8080/g' dist/index.html
+
 # Compile the server
 cd ..
 yarn
