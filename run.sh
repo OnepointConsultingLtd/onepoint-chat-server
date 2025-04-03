2428f262aabc17b6822179a404d0c18ef0356486
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-04-03 18:12:41 +0100

    Changing localhost to server ip

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ yarn build
 # Change the port variable in .\onepoint-chat-ui\dist\index.html to 8084
 sed -i 's/4000/8084/g' dist/index.html
 sed -i 's/5000/8080/g' dist/index.html
+sed -i 's/"localhost"/"176.34.128.143"/g' dist/index.html
 
 # Compile the server
 cd ..
