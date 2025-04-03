27161db261ebd5f4cd783ec559ac27a1489bd90f
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-04-03 13:50:41 +0100

    Removed dist files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chat_websocket_server_1 = require("@gilf/chat-websocket-server");
-const onepoint_1 = require("./callbacks/onepoint");
-async function main() {
-    console.log("Starting Onepoint Chat Server...");
-    (0, chat_websocket_server_1.initChatServer)([new chat_websocket_server_1.ChatCallback("onepoint", onepoint_1.onepointCallback)]);
-}
-main();
