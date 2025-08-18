31418f21c69d1489d5e78ce41dd2c162f53180d9
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-08-18 08:07:02 +0100

    Made sed to use in place replacement

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ sed -i 's/"localhost"/"176.34.128.143"/g' dist/index.html
 cp src/lib/server_staging.ts src/lib/server.ts
 
 # Replace the token with sed
-sed '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvbmVwb2ludGx0ZCIsIm5hbWUiOiJvbmVwb2ludGx0ZCIsImlhdCI6MTc1NDg1MjkzNSwiZW1haWwiOiJnaWwuZmVybmFuZGVzQG9uZXBvaW50bHRkLmNvbSIsInBlcm1pc3Npb25zIjpbInJlYWQiLCJ3cml0ZSJdfQ.UvoQl7o6A1FhOfYxwzPbOy-cRU5l3ZwrY37TAJ-bn96rqR8QcGKLbjr9K8jm84U3fYtOD3rEQl2-Y4rOM31OJw"/' src/lib/constants.ts
+sed -i '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvbmVwb2ludGx0ZCIsIm5hbWUiOiJvbmVwb2ludGx0ZCIsImlhdCI6MTc1NDg1MjkzNSwiZW1haWwiOiJnaWwuZmVybmFuZGVzQG9uZXBvaW50bHRkLmNvbSIsInBlcm1pc3Npb25zIjpbInJlYWQiLCJ3cml0ZSJdfQ.UvoQl7o6A1FhOfYxwzPbOy-cRU5l3ZwrY37TAJ-bn96rqR8QcGKLbjr9K8jm84U3fYtOD3rEQl2-Y4rOM31OJw"/' src/lib/constants.ts
 
 # Compile the server
 cd ..
