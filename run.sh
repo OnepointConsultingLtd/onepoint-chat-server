8df10708c2e02aa1ed7130c36d412dfedff0e5c4
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-08-18 08:51:26 +0100

    Merged and changed token

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ sed -i 's/localhost/176.34.128.143/g' dist/index.html
 cp src/lib/server_staging.ts src/lib/server.ts
 
 # Replace the token with sed
-sed -i '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJheml6aV9iYW5rIiwibmFtZSI6ImF6aXppX2JhbmsiLCJpYXQiOjE3NDk3NDU4NjUsImVtYWlsIjoibXVydGF6YS5oYXNzYW5pQG9uZXBvaW50bHRkLmNvbSJ9.xvKGivWBWqRc5e4iMSZ18Qls-YnpbCljYDfVF7s0zpiHFEMmOIQHkWOf9tc_cOhP7eKjeKdFE0tgM1g5vvMzfg"/' src/lib/constants.ts
+sed -i '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvbmVwb2ludGx0ZCIsIm5hbWUiOiJvbmVwb2ludGx0ZCIsImlhdCI6MTc1NTUwMzMxMiwiZW1haWwiOiJvc2NhQG9uZXBvaW50bHRkLmNvbSIsInBlcm1pc3Npb25zIjpbInJlYWQiXX0.0r1g_L5WNN8bdiy58mTKn8Ktt3OUj6ZI6nbFJVSeJbdwwFJT6pe2pX4crLyUR-zprePcVIgjIpol320hB-7ksw"/' src/lib/constants.ts
 
 # Compile the server
 cd ..
