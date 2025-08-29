23c83f2b71d53ede090289d1b1ea3574571738ec
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-08-29 09:02:58 +0100

    Moved changes to source files before build

--- run.sh
+++ run.sh
@@ -8,6 +8,12 @@ cd "$(dirname "$0")"
 # Set yarn path
 YARN_PATH='/home/ubuntu/.nvm/versions/node/v22.12.0/bin/yarn'
 
+# Replace the server with the staging server
+cp src/lib/server_staging.ts src/lib/server.ts
+
+# Replace the token with sed
+sed -i '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvbmVwb2ludGx0ZCIsIm5hbWUiOiJvbmVwb2ludGx0ZCIsImlhdCI6MTc1NTUwMzMxMiwiZW1haWwiOiJvc2NhQG9uZXBvaW50bHRkLmNvbSIsInBlcm1pc3Npb25zIjpbInJlYWQiXX0.0r1g_L5WNN8bdiy58mTKn8Ktt3OUj6ZI6nbFJVSeJbdwwFJT6pe2pX4crLyUR-zprePcVIgjIpol320hB-7ksw"/' src/lib/constants.ts
+
 # Compile the UI
 cd onepoint-chat-ui
 $YARN_PATH
@@ -17,12 +23,6 @@ $YARN_PATH build
 sed -i 's/4000/443/g' dist/index.html
 sed -i 's/5000/443/g' dist/index.html
 sed -i 's/localhost/osca.onepointltd.ai/g' dist/index.html
-
-# Replace the server with the staging server
-cp src/lib/server_staging.ts src/lib/server.ts
-
-# Replace the token with sed
-sed -i '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvbmVwb2ludGx0ZCIsIm5hbWUiOiJvbmVwb2ludGx0ZCIsImlhdCI6MTc1NTUwMzMxMiwiZW1haWwiOiJvc2NhQG9uZXBvaW50bHRkLmNvbSIsInBlcm1pc3Npb25zIjpbInJlYWQiXX0.0r1g_L5WNN8bdiy58mTKn8Ktt3OUj6ZI6nbFJVSeJbdwwFJT6pe2pX4crLyUR-zprePcVIgjIpol320hB-7ksw"/' src/lib/constants.ts
 
 # Compile the server
 cd ..
