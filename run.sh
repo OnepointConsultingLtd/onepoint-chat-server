c64f45678e8230fe8d28d5367ffbb9b528288d2f
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-12-02 20:56:03 +0000

    Corrected run.sh script

--- run.sh
+++ run.sh
@@ -10,13 +10,20 @@ YARN_PATH='/home/ubuntu/.nvm/versions/node/v22.12.0/bin/yarn'
 
 # Compile the UI
 cd onepoint-chat-ui
+
+# Replace the server with the staging server
+cp src/lib/server_staging.ts src/lib/server.ts
+
+# Replace the token with sed
+sed -i '/ONE_TIME_TOKEN/s/"[^"]*"/"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvbmVwb2ludGx0ZCIsIm5hbWUiOiJvbmVwb2ludGx0ZCIsImlhdCI6MTc1NTUwMzMxMiwiZW1haWwiOiJvc2NhQG9uZXBvaW50bHRkLmNvbSIsInBlcm1pc3Npb25zIjpbInJlYWQiXX0.0r1g_L5WNN8bdiy58mTKn8Ktt3OUj6ZI6nbFJVSeJbdwwFJT6pe2pX4crLyUR-zprePcVIgjIpol320hB-7ksw"/' src/lib/constants.ts
+
 $YARN_PATH
 $YARN_PATH build
 
 # Change the port variable in .\onepoint-chat-ui\dist\index.html to 8084
-sed -i 's/4000/8084/g' dist/index.html
-sed -i 's/5000/8080/g' dist/index.html
-sed -i 's/"localhost"/"176.34.128.143"/g' dist/index.html
+sed -i 's/4000/443/g' dist/index.html
+sed -i 's/5000/443/g' dist/index.html
+sed -i 's/localhost/osca.onepointltd.ai/g' dist/index.html
 
 # Compile the server
 cd ..
