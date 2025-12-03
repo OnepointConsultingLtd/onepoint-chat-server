313c6f82f4ab7bb4f115016c3bef1d6238c76a06
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-12-03 08:39:01 +0000

    Fixed Osca name in initial message.

--- onepoint-chat-ui/src/lib/constants.ts
+++ onepoint-chat-ui/src/lib/constants.ts
@@ -1,6 +1,6 @@
 // constants.ts
 export const INITIAL_MESSAGE =
-  "Welcome to Onepoint! I'm OSCA, your AI Advisor. What challenge or opportunity in d";
+  "Welcome to Onepoint! I'm Osca, your AI Advisor. What challenge or opportunity in d";
 
 export const PROJECT_INFO = {
   NAME: 'Osca',
